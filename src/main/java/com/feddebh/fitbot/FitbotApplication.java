40dd246ac33f99f5d9cf4e56f2d01238bce4468b
feddebh <bluisfederico@gmail.com>
2024-10-12 11:00:47 -0300

    estructura inicial del proyecto. Creacion de nueva rama

---
+++ src/main/java/com/feddebh/fitbot/FitbotApplication.java
@@ -0,0 +1,13 @@
+package com.feddebh.fitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feddebh/fitbot/FitbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feddebh.fitbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
