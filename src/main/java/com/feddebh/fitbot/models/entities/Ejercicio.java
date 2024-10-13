861b975502c51cc71c3fd59adb148709a8c194ea
feddebh <bluisfederico@gmail.com>
2024-10-13 09:09:58 -0300

    Modified app.properties, added data to models.

--- src/main/java/com/feddebh/fitbot/models/entities/Ejercicio.java
+++ src/main/java/com/feddebh/fitbot/models/entities/Ejercicio.java
@@ -1,4 +1,30 @@
 package com.feddebh.fitbot.models.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "ejercicios")
+@Data
 public class Ejercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String tipo; // Ej. "fuerza", "cardio"
+
+    @Column(nullable = false)
+    private int duracion; // Duración en minutos
+
+
+
+
 }
--- src/main/java/com/feddebh/fitbot/models/entities/Usuario.java
+++ src/main/java/com/feddebh/fitbot/models/entities/Usuario.java
@@ -1,4 +1,21 @@
 package com.feddebh.fitbot.models.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+
 public class Usuario {
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL usará auto-incremento
+    private Long id;
+
+    @Column(nullable = false) // Hace que el campo sea obligatorio en la base de datos
+    private String nombre;
+
+    @Column(nullable = false)
+    private String nivel; // Ej. "principiante", "intermedio", "avanzado"
+
 }
